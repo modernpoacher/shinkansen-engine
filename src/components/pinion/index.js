6bc310a8cc52f35c0a2ded1fe9df2f8a06b66f90
Jonathan Perry <jonathanperry@sequencemedia.net>
2020-06-08 15:26:46 +0100

    Updated packages

--- src/components/pinion/index.js
+++ src/components/pinion/index.js
@@ -1,19 +1,9 @@
 export {
-  CheckAnswersSprocket as CheckAnswers
-} from 'shinkansen-sprockets/lib'
-
-export {
-  ErrorSummarySprocket as ErrorSummary
-} from 'shinkansen-sprockets/lib'
-
-/*
-export {
   CheckAnswers
-} from 'shinkansen-pinion/lib/components/check-answers'
+} from 'shinkansen-pinion/lib/to-check-answers'
 
 export {
   ErrorSummary
-} from 'shinkansen-pinion/lib/components/error-summary'
-*/
+} from 'shinkansen-pinion/lib/to-error-summary'
 
 export default from 'shinkansen-pinion/lib/components/pinion'
