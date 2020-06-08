b79e77c846369a1809df3f3d91069ee30327b965
Jonathan Perry <jonathanperry@sequencemedia.net>
2020-06-08 15:37:28 +0100

    `CheckAnswers` `ErrorSummary`

--- src/components/pinion/index.js
+++ src/components/pinion/index.js
@@ -1,9 +1,5 @@
-export {
-  CheckAnswers
-} from 'shinkansen-pinion/lib/to-check-answers'
+export CheckAnswers from 'shinkansen-pinion/lib/components/check-answers'
 
-export {
-  ErrorSummary
-} from 'shinkansen-pinion/lib/to-error-summary'
+export ErrorSummary from 'shinkansen-pinion/lib/components/error-summary'
 
 export default from 'shinkansen-pinion/lib/components/pinion'
