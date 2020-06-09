aeeebd377d62eba221ef89e6b6f26d602613ddbb
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-09 18:58:02 +0100

    `Pantograph`

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export Cogs from './cogs'
 export Gears from './gears'
+export Pantograph from './pantograph'
 export Pinion from './pinion'
 export Rails from './rails'
 export Signals from './signals'
