3a040fc325e8b7b8ca9b077e3fb8cd51819c4f4e
Jonathan Perry <jonathanperry@modernpoacher.com>
2023-05-06 19:12:28 +0100

    Updated `package.json` &/ `package-lock.json`

--- src/transformers/pinion/common/index.mjs
+++ src/transformers/pinion/common/index.mjs
@@ -5,4 +5,3 @@ const log = debug('shinksansen-engine')
 log('`shinkansen` is awake')
 
 export * from 'shinkansen-pinion/transformers/common'
-export { default } from 'shinkansen-pinion/transformers/common'
