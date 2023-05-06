9e81c540d661bab015e9f1f190a432d764cbb157
Jonathan Perry <jonathanperry@modernpoacher.com>
2023-05-06 01:34:04 +0100

    Reorganisation

--- src/components/pinion/index.cjs
+++ src/components/pinion/index.cjs
@@ -1,11 +1,7 @@
 require('@babel/register')
 
-const CheckAnswers = require('shinkansen-pinion/components/check-answers')
-const ErrorSummary = require('shinkansen-pinion/components/error-summary')
 const {
   default: Pinion
 } = require('shinkansen-pinion/components/pinion')
 
-module.exports.CheckAnswers = CheckAnswers
-module.exports.ErrorSummary = ErrorSummary
-module.exports.Pinion = Pinion
+module.exports = Pinion
