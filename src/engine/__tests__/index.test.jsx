0391e467a633fedfa2b8504c45320b2c818718e9
Jonathan Perry <jonathanperry@modernpoacher.com>
2023-05-15 03:08:54 +0100

    `MockLink`

--- src/engine/__tests__/index.test.jsx
+++ src/engine/__tests__/index.test.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import renderer from 'react-test-renderer'
 
 import Component from 'shinkansen-engine/engine'
@@ -59,7 +60,33 @@ const GEARS = {
   pattern: '/:alpha/:omega'
 }
 
-jest.mock('react-router-dom')
+function MockLink ({ to, children }) {
+  return (
+    <a href={to} className='mock-link'>
+      {children}
+    </a>
+  )
+}
+
+MockLink.propTypes = {
+  to: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape()
+  ]),
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(
+      PropTypes.node
+    )
+  ])
+}
+
+jest.mock('react-router-dom', () => {
+  return {
+    __esModule: true,
+    Link: MockLink
+  }
+})
 
 describe('shinkansen-engine/engine', () => {
   describe('<Component />', () => {
