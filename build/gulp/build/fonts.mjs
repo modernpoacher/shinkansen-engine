5366b2a6de499de6a205ac9d132c1991036539cf
Jonathan Perry <jonathanperry@modernpoacher.com>
2023-06-02 16:10:59 +0100

    `@sequencemedia/gulp-debug`

--- build/gulp/build/fonts.mjs
+++ build/gulp/build/fonts.mjs
@@ -1,6 +1,6 @@
 import path from 'path'
 import gulp from '@sequencemedia/gulp'
-import debug from 'gulp-debug'
+import debug from '@sequencemedia/gulp-debug'
 import vinylPaths from 'vinyl-paths'
 import { deleteAsync as del } from 'del'
 
